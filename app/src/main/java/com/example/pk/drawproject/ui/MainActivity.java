b1351f54ddbd18b266d04abe646bdcfb45ce03f3
Ruslan <yesinruslan@gmail.com>
2016-10-30 17:35:29 +0200

    update MainActivity.class

--- app/src/main/java/com/example/pk/drawproject/ui/MainActivity.java
+++ app/src/main/java/com/example/pk/drawproject/ui/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.pk.drawproject.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.pk.drawproject.R;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     ViewPager viewPager;
     ImageButton searchButton;
-    private String[] scope = new String[]{VKScope.MESSAGES, VKScope.FRIENDS, VKScope.WALL, VKScope.AUDIO};
+    private String[] scope = new String[]{VKScope.AUDIO};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,19 +27,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         searchButton = (ImageButton) findViewById(R.id.searchButton);
         searchButton.setOnClickListener(this);
         if (!VKSdk.isLoggedIn()) {
-            VKSdk.login(this, scope);
             showUI();
         } else {
-            showUI();
+            VKSdk.login(this, scope);
+        }
+    }
+
+    private void showUI() {
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                showUI();
+            }
+
+            @Override
+            public void onError(VKError error) {
+// Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
     @Override
     public void onClick(View v) {
 
-    }
-    private void showUI(){
-        viewPager = (ViewPager) findViewById(R.id.viewPager);
-        viewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
     }
 }
